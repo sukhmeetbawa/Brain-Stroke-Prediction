32b32a77c997eff527f709a75f40b8660a2f0ff1
Sukhmeet <86137661+sukhmeetbawa@users.noreply.github.com>
2023-04-25 04:26:20 +0530

    Cleanups and Make Prediction Responsive

--- frontend/src/pages/Prediction.js
+++ frontend/src/pages/Prediction.js
@@ -1,46 +1,37 @@
 // Importing modules
-import React, { useState, useEffect, Component } from "react";
+import React, { useState } from "react";
 import Axios from "axios";
+import {
+  FormControl,
+  InputLabel,
+  Select,
+  MenuItem,
+  Button,
+  Container,
+  Typography,
+  Input,
+  Box,
+  Grid,
+} from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
 
 function Prediction() {
   //Using useState to set incoming variable from Python backend
   const [data, setdata] = useState([{}]);
   const [name, setName] = useState("");
+  const [age, setAge] = useState("");
+  const [glucose, setGlucose] = useState("");
+  const [bmi, setBmi] = useState("");
+  const [heart, setHeart] = useState("");
+  const [hypertension, setHypertension] = useState("");
+  const [gender, setGender] = useState("");
+  const [marital, setMaritalStatus] = useState("");
+  const [work, setWorkStatus] = useState("");
+  const [residence, setResidenceStatus] = useState("");
+  const [smoking, setSmokingStatus] = useState("");
 
-  function sendUserInfo() {
-    let name = document.getElementById("name").value;
-    let gender = document.getElementById("gender").value;
-    let age = document.getElementById("age").value;
-    let hypertension = document.getElementById("hypertension").value;
-    let heart = document.getElementById("heart").value;
-    let marital = document.getElementById("marital").value;
-    let work = document.getElementById("work").value;
-    let residence = document.getElementById("residence").value;
-    let glucose = document.getElementById("glucose").value;
-    let bmi = document.getElementById("bmi").value;
-    let smoking = document.getElementById("smoking").value;
-
-    //SETTING NAME TO CURRENT NAME FOR MONGODB
-    setName(name);
-
-    //FORM VALIDATION
-    if (
-      name.trim().length == 0 ||
-      gender.trim().length == 0 ||
-      age.trim().length == 0 ||
-      hypertension.trim().length == 0 ||
-      heart.trim().length == 0 ||
-      marital.trim().length == 0 ||
-      work.trim().length == 0 ||
-      residence.trim().length == 0 ||
-      glucose.trim().length == 0 ||
-      bmi.trim().length == 0 ||
-      smoking.trim().length == 0
-    ) {
-      alert("Please enter all fields!");
-      return;
-    }
-
+  const sendUserInfo = (event) => {
+    event.preventDefault();
     let userInfo = [];
     userInfo.push(
       name,
@@ -63,183 +54,197 @@ function Prediction() {
     request.send();
 
     //fetching using fetch function; i.e. GET request
-    fetch(`/ProcessUserinfo/${JSON.stringify(userInfo)}`).then((res) =>
-      res.json().then((data) => {
-        setdata(data);
-        var stringified = JSON.stringify(data);
-        var parsedObj = JSON.parse(stringified);
+    fetch(`/ProcessUserinfo/${JSON.stringify(userInfo)}`, { method: "POST" })
+      .then((res) => res.json())
+      .then((parsedObj) => {
         console.log(parsedObj);
 
-        //BACKEND MONGODB TO STORE STROKE CHANCES WITH USERNAME
-        Axios.post("http://localhost:3001/insert", {
+        Axios.post(`http://localhost:3001/insert`, {
           userName: name,
           strokeStatus: parsedObj,
         });
 
-        if (parsedObj == "No stroke")
+        if (parsedObj === "No stroke") {
           alert("Congratulations! Your chances of a stroke are low.");
-        else
+        } else {
           alert(
             "Your chances of getting a stroke are high, please consult a doctor!"
           );
-      })
-    );
-  }
+        }
+      });
+  };
 
   return (
-    <div className="App">
-      <div className="form-box">
+    <Container maxWidth="xs">
+      <Box>
+        <Typography component="h2" variant="h4" align="center">
+          Enter Details
+        </Typography>
         <form>
-          <div className="form-container">
-            <h1>ENTER &nbsp; DETAILS</h1>
-            <h1>TO &nbsp; FIND &nbsp; CHANCES &nbsp; OF &nbsp; STROKE</h1>
-            <br></br>
-            <h3>NAME:</h3>
-            <input
-              type="text"
-              name="Name"
-              id="name"
-              placeholder="Name"
-              required
-            ></input>
-            <br></br>
-            <h3>GENDER:</h3>
-            <input
-              type="text"
-              id="gender"
-              placeholder="Gender"
-              list="genders"
-              required
-            />
-            <datalist id="genders">
-              <option>Male</option>
-              <option>Female</option>
-            </datalist>
-            <br></br>
-            <h3>AGE:</h3>
-            <input
-              type="number"
-              name="Age"
-              id="age"
-              placeholder="Age"
-              required
-            ></input>
-            <br></br>
-            <h3>HYPERTENSION:</h3>
-            <input
-              type="text"
-              id="hypertension"
-              placeholder="Hypertension (1 if true 0 if false)"
-              list="hyper"
-              required
-            ></input>
-            <br></br>
-            <datalist id="hyper">
-              <option>0</option>
-              <option>1</option>
-            </datalist>
-            <br></br>
-            <h3>HEART DISEASE:</h3>
-            <input
-              type="text"
-              id="heart"
-              placeholder="Heart disease (1 if true 0 if false)"
-              list="heartlist"
-              required
-            ></input>
-            <br></br>
-            <datalist id="heartlist">
-              <option>0</option>
-              <option>1</option>
-            </datalist>
-            <br></br>
-            <h3>MARITAL STATUS:</h3>
-            <input
-              type="text"
-              id="marital"
-              placeholder="Marital Status"
-              list="maritallist"
-              required
-            ></input>
-            <br></br>
-            <datalist id="maritallist">
-              <option>Yes</option>
-              <option>No</option>
-            </datalist>
-            <br></br>
-            <h3>WORK STATUS:</h3>
-            <input
-              type="text"
-              id="work"
-              placeholder="Work status"
-              list="worklist"
-              required
-            ></input>
-            <br></br>
-            <datalist id="worklist">
-              <option>Private</option>
-              <option>Self-employed</option>
-              <option>Govt-job</option>
-              <option>Children</option>
-              <option>Never worked</option>
-            </datalist>
-            <br></br>
-            <h3>RESIDENCE TYPE:</h3>
-            <input
-              type="text"
-              id="residence"
-              placeholder="Residence type"
-              list="residencelist"
-              required
-            ></input>
-            <br></br>
-            <datalist id="residencelist">
-              <option>Urban</option>
-              <option>Rural</option>
-            </datalist>
-            <br></br>
-            <h3>GLUCOSE LEVEL:</h3>
-            <input
-              type="number"
-              name="Glucose"
-              id="glucose"
-              placeholder="Glucose Level"
-              required
-            ></input>
-            <br></br>
-            <h3>BMI:</h3>
-            <input
-              type="number"
-              name="BMI"
-              id="bmi"
-              placeholder="BMI"
-              required
-            ></input>
-            <br></br>
-            <h3>SMOKING STATUS:</h3>
-            <input
-              type="text"
-              id="smoking"
-              placeholder="Smoking Status"
-              list="smokinglist"
-              required
-            ></input>
-            <br></br>
-            <datalist id="smokinglist">
-              <option>smokes</option>
-              <option>never smoked</option>
-              <option>formerly smoked</option>
-              <option>Unknown</option>
-            </datalist>
-            <br></br>
-
-            <button type="button" className="submit" onClick={sendUserInfo}>
-              Submit
-            </button>
-          </div>
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="name">Name</InputLabel>
+                <Input
+                  id="name"
+                  type="text"
+                  inputProps={{ min: 0 }}
+                  required
+                  onChange={(event) => setName(event.target.value)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="gender">Gender</InputLabel>
+                <Select
+                  id="gender"
+                  name="gender"
+                  required
+                  value={gender}
+                  onChange={(event) => setGender(event.target.value)}
+                >
+                  <MenuItem value="Male">Male</MenuItem>
+                  <MenuItem value="Female">Female</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="age">Age</InputLabel>
+                <Input
+                  id="age"
+                  type="number"
+                  inputProps={{ min: 0 }}
+                  required
+                  onChange={(event) => setAge(event.target.value)}
+                />
+              </FormControl>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="hypertension">Hypertension</InputLabel>
+                <Select
+                  id="hypertension"
+                  value={hypertension}
+                  onChange={(event) => setHypertension(event.target.value)}
+                  required
+                >
+                  <MenuItem value={0}>False</MenuItem>
+                  <MenuItem value={1}>True</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="heart">Heart Disease</InputLabel>
+                <Select
+                  id="heart"
+                  value={heart}
+                  onChange={(e) => setHeart(e.target.value)}
+                  required
+                >
+                  <MenuItem value={0}>False</MenuItem>
+                  <MenuItem value={1}>True</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="marital">Marital Status</InputLabel>
+                <Select
+                  id="marital"
+                  name="marital"
+                  required
+                  onChange={(e) => setMaritalStatus(e.target.value)}
+                >
+                  <MenuItem value="Yes">Yes</MenuItem>
+                  <MenuItem value="No">No</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="work">Work Status</InputLabel>
+                <Select
+                  id="work"
+                  name="work"
+                  required
+                  onChange={(e) => setWorkStatus(e.target.value)}
+                >
+                  <MenuItem value="Private">Private Job</MenuItem>
+                  <MenuItem value="Self-employed">Self Employed</MenuItem>
+                  <MenuItem value="Govt-job">Government Job</MenuItem>
+                  <MenuItem value="Children">Housekeeper</MenuItem>
+                  <MenuItem value="Never worked">Never Worked</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="residence">Residence Status</InputLabel>
+                <Select
+                  id="residence"
+                  name="residence"
+                  required
+                  onChange={(e) => setResidenceStatus(e.target.value)}
+                >
+                  <MenuItem value="Urban">Urban</MenuItem>
+                  <MenuItem value="Rural">Rural</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="glucose">Glucose Level</InputLabel>
+                <Input
+                  id="glucose"
+                  type="number"
+                  inputProps={{ min: 0 }}
+                  required
+                  onChange={(event) => setGlucose(event.target.value)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="bmi">Body Mass Index</InputLabel>
+                <Input
+                  id="bmi"
+                  type="number"
+                  inputProps={{ min: 0 }}
+                  required
+                  onChange={(event) => setBmi(event.target.value)}
+                />
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <FormControl fullWidth margin="normal">
+                <InputLabel htmlFor="smoking">Smoking Status</InputLabel>
+                <Select
+                  id="smoking"
+                  name="smoking"
+                  required
+                  onChange={(e) => setSmokingStatus(e.target.value)}
+                >
+                  <MenuItem value="smokes">Active Smoker</MenuItem>
+                  <MenuItem value="formerly smoked">Former Smoker</MenuItem>
+                  <MenuItem value="never smoked">Never Smoked</MenuItem>
+                  <MenuItem value="Unknown">Unknown</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                color="primary"
+                onClick={sendUserInfo}
+              >
+                Submit
+              </Button>
+            </Grid>
+          </Grid>
         </form>
-      </div>
-    </div>
+      </Box>
+    </Container>
   );
 }
 
